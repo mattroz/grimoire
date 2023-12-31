9cb9a8b32b0f20d5760dc157afa3f6d869df0d28
mattroz <matveyrozanov@gmail.com>
2023-12-31 14:04:47 +0100

    Implemented masking in self-attention (only casual for now); added TransformerEncoderBlock and MaskedMHA

--- grimoire/core/layers/transformers.py
+++ grimoire/core/layers/transformers.py
@@ -49,21 +49,27 @@ class SelfAttentionBlock(nn.Module):
         # W_k is [d_model, d_k], W_q is [d_model, d_k], W_v is [d_model, d_v]
         self.query_projection = nn.Linear(d_model, embedding_dim, bias=False)
         self.key_projection = nn.Linear(d_model, embedding_dim, bias=False)
-        self.value_projection = nn.Linear(d_model, embedding_dim, bias=False)
+        self.value_projection = nn.Linear(d_model, embedding_dim, bias=True)
 
         self.final_projection = nn.Linear(embedding_dim, embedding_dim, bias=False)
         self.norm = nn.LayerNorm(embedding_dim)
 
-    def forward(self, input_tensor):
+    def forward(self, query, key, value, attention_mask: torch.BoolTensor = None):
         # Get linear projections of K, Q and V according to Fig. 2 in the original Transformer paper
-        queries = self.query_projection(input_tensor)  # [b, seqlen, d_k]
-        keys = self.key_projection(input_tensor)       # [b, seqlen, d_k]
-        values = self.value_projection(input_tensor)   # [b, seqlen, d_v]
+        queries = self.query_projection(query)  # [b, seqlen, d_k]
+        keys = self.key_projection(key)       # [b, seqlen, d_k]
+        values = self.value_projection(value)   # [b, seqlen, d_v]
 
         # Perform Scaled Dot-Product Attention (eq. 1 in the Transformer paper).
         # Each SDPA block yields tensor of size d_v.
         scale = self.d_keys ** -0.5
-        attention_scores = torch.softmax(torch.matmul(queries, keys.transpose(-1, -2)) * scale, dim=-1) # [b, seqlen, seqlen]
+        attention_scores = torch.matmul(queries, keys.transpose(-1, -2)) * scale
+        
+        if attention_mask is not None:
+            assert attention_mask.dtype == torch.bool
+            attention_scores = attention_scores.masked_fill(attention_mask.logical_not(), float("-inf"))
+
+        attention_scores = torch.softmax(attention_scores, dim=-1) # [b, seqlen, seqlen]
         attention_scores = torch.matmul(attention_scores, values)   # [b, seqlen, d_v]
 
         return attention_scores
@@ -75,8 +81,52 @@ class MultiHeadAttention(nn.Module):
         head_size = d_model // num_heads
         self.mha = nn.ModuleList([SelfAttentionBlock(d_model, head_size) for _ in range(num_heads)])
 
-    def forward(self, x):
-        x = torch.cat([head(x) for head in self.mha], dim=-1)
+    def forward(self, query, key, value):
+        x = torch.cat([head(query, key, value, attention_mask=None) for head in self.mha], dim=-1)
         
         return x
     
+
+class MaskedMultiHeadAttention(nn.Module):
+    def __init__(self, num_heads=8, d_model=512):
+        super().__init__()
+        head_size = d_model // num_heads
+        self.mha = nn.ModuleList([SelfAttentionBlock(d_model, head_size) for _ in range(num_heads)])
+
+    def forward(self, query, key, value):
+        """Note: attention_mask might be in a so-called 'casual' mode (triangluar matrix with 1s on the lower left corner)
+        to prevent model from looking into the future, and it also might be 'custom', if you want to mask out
+        some specific tokens (e.g. padding tokens). 
+        In this implementation we only use 'casual' attention mask for now. 
+        """
+        seqlen = query.size(-2)
+        attention_mask = torch.ones(seqlen, seqlen, dtype=torch.bool).tril(diagonal=0)
+        x = torch.cat([head(query, key, value, attention_mask=attention_mask) for head in self.mha], dim=-1)
+        
+        return x
+
+
+class TransformerEncoderBlock(nn.Module):
+    """Section 3.3 from "Attention Is All You Need" paper"""
+    def __init__(self, d_model=512, num_heads=8, d_ff=2048, dropout=0.1):
+        super().__init__()
+        self.mha = MultiHeadAttention(num_heads, d_model)
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, d_ff),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(d_ff, d_model)
+        )
+        self.mha_norm = nn.LayerNorm(d_model)
+        self.ffn_norm = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, input_tensor):
+        x = input_tensor
+        x = self.mha(query=x, key=x, value=x)
+        x = self.dropout(x)
+        x = self.mha_norm(x + input_tensor)
+        x = self.ffn_norm(x + self.dropout(self.ffn(x)))
+
+        return x
+        
